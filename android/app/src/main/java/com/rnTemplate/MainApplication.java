68ecda767dedc0920f001b2291dd17b1954e51c9
yuanxiaowei <yuanxiaowei@chuangjiangx.com>
2021-04-10 18:23:12 +0800

    android icon

--- android/app/src/main/java/com/rnTemplate/MainApplication.java
+++ android/app/src/main/java/com/rnTemplate/MainApplication.java
@@ -5,9 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.bridge.JSIModulePackage;
-import com.github.kevinejohn.keyevent.KeyEventPackage;
 import com.rnTemplate.JSIPackageList;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          //  packages.add(new KeyEventPackage());
+
           return packages;
         }
 
