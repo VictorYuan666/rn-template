166d7ffa273c1ee69a45f9e7605e890cb3ce142f
O小v <yuanxiaowei1991@gmail.com>
2021-05-16 17:49:51 +0800

    Update splash.style.js

--- src/pages/guide/splash/splash.style.js
+++ src/pages/guide/splash/splash.style.js
@@ -1,11 +1,1 @@
-import EStyleSheet from '@themes';
 
-const styles = EStyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-
-export default styles;
