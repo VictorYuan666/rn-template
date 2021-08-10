aafbc35ddb252ca46ec7ca83ec855991746e6ba4
yuanxiaowei <yuanxiaowei1991@gmail.com>
2021-08-11 00:08:06 +0800

    feat: remove mmkv

--- android/app/src/main/java/com/rntemplate/JSIPackageList.java
+++ android/app/src/main/java/com/rntemplate/JSIPackageList.java
@@ -4,7 +4,7 @@ import com.facebook.react.bridge.JSIModuleSpec;
 import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
-import com.reactnativemmkv.MmkvModule;
+// import com.reactnativemmkv.MmkvModule;
 
 import java.util.Collections;
 import java.util.List;
@@ -14,7 +14,7 @@ public class JSIPackageList extends ReanimatedJSIModulePackage {
   @Override
   public List<JSIModuleSpec> getJSIModules(ReactApplicationContext reactApplicationContext, JavaScriptContextHolder jsContext) {
     super.getJSIModules(reactApplicationContext, jsContext);
-    MmkvModule.install(jsContext, reactApplicationContext.getFilesDir().getAbsolutePath() + "/mmkv");
+    // MmkvModule.install(jsContext, reactApplicationContext.getFilesDir().getAbsolutePath() + "/mmkv");
     return Collections.emptyList();
   }
 }
